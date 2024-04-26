6af01ee368d7496b39a3b879f5003a31ec3c1c86
无涯 <1619686166@qq.com>
2024-04-26 10:56:17 +0800

    feat: 更新代码

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { default as toBarLine } from './toBarLine';
-export { default as toStackedBar } from './toStackedBar';
 export { default as toWordCloud } from './toWordCloud';
+export { default as toStackedBar } from './toStackedBar';
